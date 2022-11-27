2adac251a3c5764841e0344392fb5893b6f4ba19
Wow77 <francmaki14@gmail.com>
2022-11-27 17:57:06 +0300

    Clean the code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,11 +1,6 @@
 
 console.log("Welcome to Rock, Paper and Scissor game.\nSelect your option: \n1.Rock  2.Paper  3.Scissor");
-/*
-//Computer selects its option
-const choiceNumber = Math.floor(Math.random()*3);
-const computerOptions = ['Rock','Paper','Scissor'];
-const computerChoice = computerOptions[choiceNumber];
-*/
+
 //Computer playing function
 function getComputerChoice(){
     const choiceNumber = Math.floor(Math.random()*3);
@@ -16,56 +11,35 @@ function getComputerChoice(){
 
 const computerSelection = getComputerChoice();
 
+//One round play funciton
+function playRound(X,Y){
+    //The player selects his/her option
+let playerSelection = prompt("Enter your option:");
+//Deciding the winner
+if (playerSelection === computerSelection) {
+    console.log("Its a Draw!");
+} else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+    console.log("You lost! Paper beats Rock");
+} else if (playerSelection == 'Rock' && computerSelection == 'Scissor'){
+    console.log("You won! Rock beats Scissor");
+}else if (playerSelection == 'Paper' && computerSelection == 'Scissor') {
+    console.log("You lost! Scissor beats Paper");
+} else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
+    console.log("You won! Rock beats Paper");
+}else if (playerSelection == 'Scissor' && computerSelection == 'Rock') {
+    console.log("You lost! Rock beats Scissor");
+} else if (playerSelection == 'Scissor' && computerSelection == 'Paper'){
+    console.log("You won! Scissor beats Paper");
+} else {
+    console.log("Invalid Chice please type one of the chice as displayed e.g Paper");
+}
+}
+
+//Five rounds play function
 function game(){
     for (let i=0; i<5; i++){
-        function playRound(X,Y){
-            //The player selects his/her option
-        let playerSelection = prompt("Enter your option:");
-        //Deciding the winner
-        if (playerSelection === computerSelection) {
-            console.log("Its a Draw!");
-        } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-            console.log("You lost! Paper beats Rock");
-        } else if (playerSelection == 'Rock' && computerSelection == 'Scissor'){
-            console.log("You won! Rock beats Scissor");
-        }else if (playerSelection == 'Paper' && computerSelection == 'Scissor') {
-            console.log("You lost! Scissor beats Paper");
-        } else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
-            console.log("You won! Rock beats Paper");
-        }else if (playerSelection == 'Scissor' && computerSelection == 'Rock') {
-            console.log("You lost! Rock beats Scissor");
-        } else if (playerSelection == 'Scissor' && computerSelection == 'Paper'){
-            console.log("You won! Scissor beats Paper");
-        } else {
-            console.log("Invalid Chice please type one of the chice as displayed e.g Paper");
-        }
-        }
-        
         playRound();
     }
 }
 
 game();
-
-/*
-//The player selects his/her option
-let playerSelection = prompt("Enter your option:");
-//Deciding the winner
-if (playerSelection === computerChoice) {
-    console.log("Its a Draw!");
-} else if (playerSelection == 'Rock' && computerChoice == 'Paper') {
-    console.log("Computer Won");
-} else if (playerSelection == 'Rock' && computerChoice == 'Scissor'){
-    console.log("You won");
-}else if (playerSelection == 'Paper' && computerChoice == 'Scissor') {
-    console.log("Computer Won");
-} else if (playerSelection == 'Paper' && computerChoice == 'Rock'){
-    console.log("You won");
-}else if (playerSelection == 'Scissor' && computerChoice == 'Rock') {
-    console.log("Computer Won");
-} else if (playerSelection == 'Scissor' && computerChoice == 'Paper'){
-    console.log("You won");
-} else {
-    console.log("Invalid Chice please type one of the chice as displayed e.g Paper");
-}
-*/
