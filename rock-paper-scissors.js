1493b89de8344f6d8e5ed2f3a7af5f60a8106795
Wow77 <francmaki14@gmail.com>
2022-11-27 16:42:12 +0300

    Create game function and playRound function The game function make the user to play the game five times. The playRound function allows the user to play the geme one round and decide who won.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,6 @@
-/*
+
 console.log("Welcome to Rock, Paper and Scissor game.\nSelect your option: \n1.Rock  2.Paper  3.Scissor");
+/*
 //Computer selects its option
 const choiceNumber = Math.floor(Math.random()*3);
 const computerOptions = ['Rock','Paper','Scissor'];
@@ -12,6 +13,39 @@ function getComputerChoice(){
     const computerChoice = computerOptions[choiceNumber];
     return computerChoice; 
 }
+
+const computerSelection = getComputerChoice();
+
+function game(){
+    for (let i=0; i<5; i++){
+        function playRound(X,Y){
+            //The player selects his/her option
+        let playerSelection = prompt("Enter your option:");
+        //Deciding the winner
+        if (playerSelection === computerSelection) {
+            console.log("Its a Draw!");
+        } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+            console.log("You lost! Paper beats Rock");
+        } else if (playerSelection == 'Rock' && computerSelection == 'Scissor'){
+            console.log("You won! Rock beats Scissor");
+        }else if (playerSelection == 'Paper' && computerSelection == 'Scissor') {
+            console.log("You lost! Scissor beats Paper");
+        } else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
+            console.log("You won! Rock beats Paper");
+        }else if (playerSelection == 'Scissor' && computerSelection == 'Rock') {
+            console.log("You lost! Rock beats Scissor");
+        } else if (playerSelection == 'Scissor' && computerSelection == 'Paper'){
+            console.log("You won! Scissor beats Paper");
+        } else {
+            console.log("Invalid Chice please type one of the chice as displayed e.g Paper");
+        }
+        }
+        
+        playRound();
+    }
+}
+
+game();
 
 /*
 //The player selects his/her option
